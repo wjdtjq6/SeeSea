c75bd1a21649123d9b99ecea3bbadb2908992e2f
jimmy <wjdtjq6@gmail.com>
2024-09-13 16:52:10 +0900

    Initial Commit

---
+++ SeeSea/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SeeSea
+//
+//  Created by 소정섭 on 9/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SeeSea/SeeSeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeSeaApp.swift
+//  SeeSea
+//
+//  Created by 소정섭 on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SeeSeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
