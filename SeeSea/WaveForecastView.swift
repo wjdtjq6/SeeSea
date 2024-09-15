f68dc7ec8f66a143b173e4cbc5128c0e3ff1ecd6
jimmy <wjdtjq6@gmail.com>
2024-09-15 23:57:47 +0900

    TabView구현중

---
+++ SeeSea/WaveForecastView.swift
@@ -0,0 +1,18 @@
+//
+//  WaveForecastView.swift
+//  SeeSea
+//
+//  Created by 소정섭 on 9/15/24.
+//
+
+import SwiftUI
+
+struct WaveForecastView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WaveForecastView()
+}
---
+++ SeeSea/WebCamView.swift
@@ -0,0 +1,18 @@
+//
+//  WebCamView.swift
+//  SeeSea
+//
+//  Created by 소정섭 on 9/15/24.
+//
+
+import SwiftUI
+
+struct WebCamView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebCamView()
+}
