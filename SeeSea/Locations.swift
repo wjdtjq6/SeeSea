c2b80e19979de93eeaf8d53ee5ed4fba7b5ea32b
jimmy <wjdtjq6@gmail.com>
2024-09-18 23:57:32 +0900

    Mapkit으로 현위치, 근처해변위치 버튼 구현완료

---
+++ SeeSea/Locations.swift
@@ -0,0 +1,64 @@
+//  Locations.swift
+//  SeeSea
+//
+//  Created by 소정섭 on 9/18/24.
+//
+
+import CoreLocation
+
+extension CLLocationCoordinate2D {
+    static let woljeongri: Self = .init(
+        latitude: 33.544586,
+        longitude: 126.820488
+    )
+    
+    static let songjeong: Self = .init(
+        latitude: 37.780269,
+        longitude: 128.938075
+    )
+    
+    static let gyeongpo: Self = .init(
+        latitude: 37.804333,
+        longitude: 128.909831
+    )
+    
+    static let dadaepo: Self = .init(
+        latitude: 35.046078,
+        longitude: 128.965758
+    )
+    
+    static let jinha: Self = .init(
+        latitude: 35.383564,
+        longitude: 129.345822
+    )
+    
+    static let gwakjiGwamul: Self = .init(
+        latitude: 33.450647,
+        longitude: 126.304597
+    )
+    
+    static let hyeopjae: Self = .init(
+        latitude: 33.394458,
+        longitude: 126.239967
+    )
+    
+    static let jungmunSaekdal: Self = .init(
+        latitude: 33.244794,
+        longitude: 126.411608
+    )
+    
+    static let ihoTeu: Self = .init(
+        latitude: 33.497992,
+        longitude: 126.452686
+    )
+    
+    static let samyangBlackSand: Self = .init(
+        latitude: 33.525725,
+        longitude: 126.586003
+    )
+    
+    static let hamdeokSeoubong: Self = .init(
+        latitude: 33.543442,
+        longitude: 126.669447
+    )
+}
--- SeeSea/MapView.swift
+++ SeeSea/MapView.swift
@@ -6,34 +6,84 @@
 //
 
 import SwiftUI
-import NMapsMap
+import MapKit
+import CoreLocation
 
-struct MapView: UIViewRepresentable {
-    func makeCoordinator() -> Coordinator {
-        Coordinator.shared
-    }
-    func makeUIView(context: Context) -> some UIView {
-        context.coordinator.getNaverMapView()
+struct MapView: View {
+    @State private var cameraPosition: MapCameraPosition = .automatic
+    @State private var locationManager = CLLocationManager()
+        
+    var body: some View {
+            Map(position: $cameraPosition) {
+                annotation(title: "월정리 해수욕장", coordinate: .woljeongri, wave: "0")
+                annotation(title: "삼양검은 모래 해수욕장", coordinate: .samyangBlackSand, wave: "0-1")
+                annotation(title: "송정 해수욕장", coordinate: .songjeong, wave: "1-2")
+                annotation(title: "경포 해수욕장", coordinate: .gyeongpo, wave: "2-3")
+                annotation(title: "다대포 해수욕장", coordinate: .dadaepo, wave: "3-4")
+                annotation(title: "진하 해수욕장", coordinate: .jinha, wave: "4-5")
+                annotation(title: "곽지과물 해수욕장", coordinate: .gwakjiGwamul, wave: "5-6")
+                annotation(title: "협재 해수욕장", coordinate: .hyeopjae, wave: "6-7")
+                annotation(title: "중문ㆍ색달 해수욕장", coordinate: .jungmunSaekdal, wave: "7-8")
+                annotation(title: "이호테우 해수욕장", coordinate: .ihoTeu, wave: "8-9")
+                annotation(title: "함덕서우봉 해수욕장", coordinate: .hamdeokSeoubong, wave: "9-10")
+            }
+            .onAppear {
+                if let location = locationManager.location {
+                    let region = MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 4, longitudeDelta: 4))
+                    cameraPosition = .region(region)
+                }
+            }
+            .overlay {
+                Button(action: {
+                    print("LOCATION")
+                }, label: {
+                    ZStack {
+                        Circle()
+                            .fill(.black.opacity(0.5))
+                            .frame(width: 50, height: 50)
+                        Image(systemName: "location")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .foregroundColor(.white)
+                    }
+                })
+                .frame(width: 20, height: 20)
+                .position(x: UIScreen.main.bounds.width-40, y: UIScreen.main.bounds.height-210)
+                
+                Button(action: {
+                    print("around")
+                }, label: {
+                    ZStack {
+                        Circle()
+                            .fill(.black.opacity(0.5))
+                            .frame(width: 50, height: 50)
+                        Image(systemName: "dot.scope")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .foregroundColor(.white)
+                    }
+                })
+                .frame(width: 20, height: 20)
+                .position(x: UIScreen.main.bounds.width-40, y: UIScreen.main.bounds.height-150)
+
+//
+            }
+            
+            //.frame(maxWidth: .infinity)
+        
         
     }
-    func updateUIView(_ uiView: UIViewType, context: Context) {
-        context.coordinator.setMarker(lat: 37.4797729789029, lng: 126.897860359752, title: "집")
-        context.coordinator.setMarker(lat: 33.4977677, lng: 126.44805, title: "제주서프로와 이호점")
-//        context.coordinator.setMarker(lat: <#T##Double#>, lng: <#T##Double#>, title: <#T##String#>)
-//        context.coordinator.setMarker(lat: <#T##Double#>, lng: <#T##Double#>, title: <#T##String#>)
-//        context.coordinator.setMarker(lat: <#T##Double#>, lng: <#T##Double#>, title: <#T##String#>)
-//        context.coordinator.setMarker(lat: <#T##Double#>, lng: <#T##Double#>, title: <#T##String#>)
-//        context.coordinator.setMarker(lat: <#T##Double#>, lng: <#T##Double#>, title: <#T##String#>)
-//        context.coordinator.setMarker(lat: <#T##Double#>, lng: <#T##Double#>, title: <#T##String#>)
-//        context.coordinator.setMarker(lat: <#T##Double#>, lng: <#T##Double#>, title: <#T##String#>)
-//        context.coordinator.setMarker(lat: <#T##Double#>, lng: <#T##Double#>, title: <#T##String#>)
-//        context.coordinator.setMarker(lat: <#T##Double#>, lng: <#T##Double#>, title: <#T##String#>)
-//        context.coordinator.setMarker(lat: <#T##Double#>, lng: <#T##Double#>, title: <#T##String#>)
-//        context.coordinator.setMarker(lat: <#T##Double#>, lng: <#T##Double#>, title: <#T##String#>)
-
-
-
+    func annotation(title: String, coordinate: CLLocationCoordinate2D, wave: String) -> some MapContent{
+        Annotation(title, coordinate: coordinate) {
+            Text("  \(wave)  ")
+                .bold()
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.blue)
+                .clipShape(.rect(cornerRadius: 30))
+        }
     }
+
 }
 
 #Preview {
